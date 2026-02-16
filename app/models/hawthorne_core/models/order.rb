ef1dfd832ba7b889fc186773fa5f3d65421f543b
Charlie Prezzano <charlie@hawthornethreads.com>
2026-02-16 12:38:57 -0500

    working on core gem integration

--- app/models/hawthorne_core/models/order.rb
+++ app/models/hawthorne_core/models/order.rb
@@ -9,7 +9,7 @@ module HawthorneCore
       #include ActiveModel::Attributes
 
       def self.charlie
-        puts 'here i am charlie boy 10 ...'
+        puts 'here i am charlie boy 12 ...'
         find_by(order_id: 1037193)
       end
 
