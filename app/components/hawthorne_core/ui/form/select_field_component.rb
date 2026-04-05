adeb49874d1fa30ca8e8a55f81ef05180ada3190
charlie <charlieprezzano@gmail.com>
2026-04-05 12:55:44 -0400

    v3

--- app/components/hawthorne_core/ui/form/select_field_component.rb
+++ app/components/hawthorne_core/ui/form/select_field_component.rb
@@ -1,12 +1,13 @@
 # v3.0
 
 class HawthorneCore::UI::Form::SelectFieldComponent < ViewComponent::Base
-  def initialize(form:, attribute:, label:, options:, prompt:, html_options:)
+  def initialize(form:, attribute:, label:, options:, prompt: nil, selected: nil, html_options: {})
     @form = form
     @attribute = attribute
     @label = label
     @options = options
     @prompt = prompt
+    @selected = selected
     @html_options = html_options
   end
 end
--- app/components/hawthorne_core/ui/form/text_field_component.rb
+++ app/components/hawthorne_core/ui/form/text_field_component.rb
@@ -1,12 +1,13 @@
 # v3.0
 
 class HawthorneCore::UI::Form::TextFieldComponent < ViewComponent::Base
-  def initialize(form:, attribute:, label:, placeholder: nil, value: nil, required: false, autocomplete: :off, maxlength: 100, autofocus: false)
+  def initialize(form:, attribute:, label:, placeholder: nil, value: nil, disabled: false, required: false, autocomplete: :off, maxlength: 100, autofocus: false)
     @form = form
     @attribute = attribute
     @label = label
     @placeholder = placeholder
     @value = value
+    @disabled = disabled
     @required = required
     @autocomplete = autocomplete
     @maxlength = maxlength
