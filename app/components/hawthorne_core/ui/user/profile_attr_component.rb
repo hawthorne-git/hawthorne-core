6613253463f387d30769bfe87e20714985f9b6c6
charlie <charlieprezzano@gmail.com>
2026-05-05 07:23:49 -0400

    v3

--- app/components/hawthorne_core/ui/user/profile_attr_component.rb
+++ app/components/hawthorne_core/ui/user/profile_attr_component.rb
@@ -1,8 +1,9 @@
 # v3.0
 
 class HawthorneCore::UI::User::ProfileAttrComponent < ViewComponent::Base
-  def initialize(label:, value:, url: nil)
+  def initialize(label:, description: nil, value:, url: nil)
     @label = label
+    @description = description
     @value = value
     @url = url
   end
