6575af8b40c5f7d4a06aafd5a24fae5204de4812
Charlie <charlieprezzano@gmail.com>
2026-05-31 07:52:08 -0400

    v3

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -4,10 +4,6 @@ unless Rails.env.development?
 
   Sidekiq.configure_server do |config|
     config.redis = { url: HawthorneCore::AppConfig.redis_sidekiq_url }
-    config.on(:startup) do
-      schedule = YAML.load_file(Rails.root.join('config/sidekiq_cron.yml'))
-      Sidekiq::Cron::Job.load_from_hash(schedule)
-    end
   end
 
   Sidekiq.configure_client do |config|
