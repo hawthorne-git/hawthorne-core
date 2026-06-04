0c9c05dcdea2f3e577f6b2ae3b9ec0751f70b7c9
Charlie <charlieprezzano@gmail.com>
2026-06-04 10:54:23 -0400

    v3

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,7 +1,4 @@
-# TODO: Test that it is being used
-
-# set the geocoder to timeout after 2 seconds,
-# and raise exceptions
+# set the geocoder to timeout after 2 seconds, and raise exceptions
 Geocoder.configure(
   timeout: 2,
   always_raise: [SocketError, Timeout::Error]
